ae227d53d563dde17dc24be058af190238b2984a
Alexey-beercode <alsemkovbn@gmail.com>
2024-10-18 00:20:23 +0300

    added application configuration

---
+++ src/Services/BookingService/BookingService.Presentation/Extensions/WebApplicationExtension.cs
@@ -0,0 +1,37 @@
+﻿using BookingService.Presentation.Middleware;
+
+namespace BookingService.Presentation.Extensions;
+
+public static class WebApplicationExtension
+{
+    public static void AddSwagger(this WebApplication app)
+    {
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+    }
+    public static void AddApplicationMiddleware(this WebApplication app)
+    {
+        app.UseHttpsRedirection(); 
+        app.UseRouting(); 
+        
+        app.UseCors(builder =>
+        {
+            builder.AllowAnyOrigin() 
+                .AllowAnyMethod()
+                .AllowAnyHeader()
+                .AllowCredentials();
+        }); 
+        
+        app.UseAuthentication();
+        app.UseAuthorization();
+
+        app.MapControllers();
+        
+        app.UseMiddleware<ExceptionHandlingMiddleware>();
+        
+        
+    }
+}
